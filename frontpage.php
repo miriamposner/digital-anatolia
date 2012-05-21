0c931532d38b2063321a82a783cb8cb3ead39817
miriamposner <miriam.posner@gmail.com>
2012-05-21 13:03:55 -0700

    First commit

---
+++ frontpage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+Template name: Home
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+			
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
