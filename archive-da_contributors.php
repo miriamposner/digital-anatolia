2421133b380ebca296f758bc2c8a6bb6418de48e
miriamposner <miriam.posner@gmail.com>
2012-05-21 17:02:04 -0700

    Added template for Contributors page.

---
+++ archive-da_contributors.php
@@ -0,0 +1,44 @@
+<?php
+/**
+Template name: Contributors
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+<!-- <?php
+	/* Queue the first post, that way we know
+	 * what date we're dealing with (if that is the case).
+	 *
+	 * We reset this later so we can run the loop
+	 * properly with a call to rewind_posts().
+	 */
+	if ( have_posts() )
+		the_post();
+?>
+
+			<h1 class="page-title">
+<?php if ( is_day() ) : ?>
+				<?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
+<?php elseif ( is_month() ) : ?>
+				<?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyten' ) ) ); ?>
+<?php elseif ( is_year() ) : ?>
+				<?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentyten' ) ) ); ?>
+<?php else : ?>
+				<?php _e( 'Blog Archives', 'twentyten' ); ?>
+<?php endif; ?>
+			</h1>
+-->
+
+<?php
+$args = array( 'post_type' => 'da_contributors', 'posts_per_page' => 20 );
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+	the_title();
+	echo '<div class="entry-content">';
+	the_content();
+	echo '</div>';
+endwhile; ?>
+
+<?php get_footer(); ?>
