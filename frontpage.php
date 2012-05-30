10c4e146392ef22b7aaf3fd037ce635af508fc68
miriamposner <miriam.posner@gmail.com>
2012-05-30 11:02:06 -0700

    Added image to frontpage.php.

--- frontpage.php
+++ frontpage.php
@@ -7,16 +7,11 @@ get_header(); ?>
 
 		<div id="container">
 			<div id="content" role="main">
-
+				<img src="<?php bloginfo('template_directory'); ?>/images/DigitalAnatolia_logo.png" title="<?php bloginfo('site_title'); ?>" />
 			
 			</div><!-- #content -->
 		</div><!-- #container -->
-		
-<?php
-	if (class_exists(‘Gallery’)) {
-	$Gallery = new Gallery();
-	$Gallery -> slideshow($output = true, $post_id = null);
-		} ?>
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
