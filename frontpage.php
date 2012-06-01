46a769b5c26f1da1ce2e0645fd28cac95d84b939
miriamposner <miriam.posner@gmail.com>
2012-06-01 13:30:15 -0700

    Added NEH logo

--- frontpage.php
+++ frontpage.php
@@ -7,10 +7,14 @@ get_header(); ?>
 
 		<div id="container">
 			<div id="content" role="main">
-				<img src="<?php bloginfo('template_directory'); ?>/images/DigitalAnatolia_logo.png" title="<?php bloginfo('site_title'); ?>" />
+
+				<div id="slideshow">
+					<?php echo do_shortcode('[portfolio_slideshow ID=59]');?>
+				</div><!-- #slideshow -->
 				
-				<?php echo do_shortcode('[portfolio_slideshow ID=59]');?>
-			
+				<div id="nehlogo">
+					<img src="<?php bloginfo('template_directory'); ?>/images/NEH.png" title="<?php bloginfo('site_title'); ?>" />
+				</div><!-- #nehlogo -->
 			</div><!-- #content -->
 		</div><!-- #container -->
 
