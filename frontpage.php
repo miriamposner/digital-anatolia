c8bf272347091aa598bc7ed3d1743a3cdd8ba4da
miriamposner <miriam.posner@gmail.com>
2012-06-01 11:16:55 -0700

    Took the loop back out, added Portfolio Slideshow shortcode to frontpage.php.

--- frontpage.php
+++ frontpage.php
@@ -9,14 +9,7 @@ get_header(); ?>
 			<div id="content" role="main">
 				<img src="<?php bloginfo('template_directory'); ?>/images/DigitalAnatolia_logo.png" title="<?php bloginfo('site_title'); ?>" />
 				
-				<?php
-				/* Run the loop to output the page.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-page.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'page' );
-				?>
-
+				<?php echo do_shortcode('[portfolio_slideshow ID=59]');?>
 			
 			</div><!-- #content -->
 		</div><!-- #container -->
